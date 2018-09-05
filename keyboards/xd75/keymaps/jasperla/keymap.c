1fcbc5544eaba9e85e890cbf12a00507cc919086
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2018-09-05 21:45:17 +0200

    tweaks to base layer

--- keyboards/xd75/keymaps/jasperla/keymap.c
+++ keyboards/xd75/keymaps/jasperla/keymap.c
@@ -37,15 +37,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | LOCK   | 1      | 2      | 3      | 4      | 5      | -      | `      | =      | 6      | 7      | 8      | 9      | 0      | BACKSP |
+ * | LOCK   | 1      | 2      | 3      | 4      | 5      | `      |        | 6      | 7      | 8      | 9      | 0      | -      | BACKSP |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | TAB    | Q      | W      | E      | R      | T      | [      | \      | ]      | Y      | U      | I      | O      | P      | |      |
+ * | TAB    | Q      | W      | E      | R      | T      | [      | =      | ]      | Y      | U      | I      | O      | P      |       |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
  * | ESC    | A      | S      | D      | F      | G      | HOME   | DEL    | PG UP  | H      | J      | K      | L      | ;      | '      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
- * | LSHIFT | Z      | X      | C      | V      | B      | END    | UP     | PG DN  | N      | M      | ,      | .      | /      | RSHIFT |
+ * | LSHIFT | Z      | X      | C      | V      | B      | END    |        | PG DN  | N      | M      | ,      | .      | /      | RSHIFT |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | LCTRL  | FN     | LGUI   | LALT   | SPACE  | SPACE  | LEFT   | DOWN   | RIGHT  | PT     | ENTER  | RALT   | RGUI   | PT     | RCTRL  |
+ * | LCTRL  | FN     | LGUI   | LALT   | SPACE  | SPACE  |        |        | PT     | ENTER  | ENTER  | RALT   | RGUI   | PT     | RCTRL  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  *
  *
@@ -53,11 +53,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QW] = { /* QWERTY */
-  { KC_LOCK,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_GRV,  KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC  },
-  { KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_BSLS, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PIPE  },
+  { KC_LOCK,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_GRV,  _______,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_BSPC  },
+  { KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_EQL, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS  },
   { KC_ESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_DEL,  KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT  },
-  { KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_UP,   KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT  },
-  { KC_LCTL,  MO(_FN), KC_LGUI, KC_LALT, KC_SPC,  KC_SPC, KC_LEFT, KC_DOWN, KC_RGHT, MO(_PT), KC_ENT,  KC_RALT, KC_RGUI, MO(_PT), KC_RCTL  },
+  { KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  _______, KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT  },
+  { KC_LCTL,  MO(_FN), KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,  _______, _______, MO(_PT), KC_ENT,  KC_ENT,  KC_RALT, KC_RGUI, MO(_PT), KC_RCTL  },
  },
 
 /* FUNCTION
